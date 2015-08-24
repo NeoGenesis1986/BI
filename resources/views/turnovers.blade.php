038603576d22e437129df03d4bc7dd5fd3d09d28
NeoGenesis1986 <yakoub_klai@yahoo.fr>
2015-08-24 14:31:38 +0100

    End of day commit

--- resources/views/turnovers.blade.php
+++ resources/views/turnovers.blade.php
@@ -1,53 +1,48 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Chiffre d'affaires</title>
-        <script src="{{ URL::asset('js/Chart.min.js') }}"></script>
-    </head>
-    <body>
-        <canvas id="myChart" style="width: 80%; height: 400px;"></canvas>
-        <script>
-            var serie = <?php
-                                $values = '[';
-                                for($i = 1; $i<=12; $i++) {
-                                    $value = 0;
-                                    foreach($turnovers as $turnover) {
-                                        if(date('n', strtotime($turnover->date)) == $i) {
-                                            $value = $turnover->value;
-                                            break;
-                                        }
-                                    }
-                                    $values .= $value;
-                                    if($i < 12) {
-                                        $values .= ', ';
+@extends('main_layout')
+@section('title') Chiffre d'affaires @endsection
+@section('scripts')
+    <script src="{{ URL::asset('js/Chart.min.js') }}"></script>
+@endsection
+@section('content')
+    <canvas id="myChart" style="width: 80%; height: 400px;"></canvas>
+    <script>
+        var serie = <?php
+                            $values = '[';
+                            for($i = 1; $i<=12; $i++) {
+                                $value = 0;
+                                foreach($turnovers as $turnover) {
+                                    if(date('n', strtotime($turnover->date)) == $i) {
+                                        $value = $turnover->value;
+                                        break;
                                     }
                                 }
-                                $values .= ']';
-                                echo $values;
-                        ?>;
-            var data = {
-                labels: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"],
-                datasets: [
-                    {
-                        label: "My First dataset",
-                        fillColor: "rgba(40, 11, 133, 0.5)",
-                        strokeColor: "rgba(220, 220, 220, 0.8)",
-                        highlightFill: "rgba(220, 220, 220, 0.75)",
-                        highlightStroke: "rgba(50, 50, 50, 0.5)",
-                        data: serie
-                    }
-                ]
-            };
-            var options = {
-                scaleOverride : true,
-                scaleSteps : 10,
-                scaleStepWidth : 1000000,
-                scaleStartValue : 0,
-                responsive : true
-            };
-            var ctx = document.getElementById("myChart").getContext("2d");
-            var myNewChart = new Chart(ctx).Bar(data, options);
-        </script>
-    </body>
-</html>
+                                $values .= $value;
+                                if($i < 12) {
+                                    $values .= ', ';
+                                }
+                            }
+                            $values .= ']';
+                            echo $values;
+                    ?>;
+        var data = {
+            labels: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+            datasets: [{
+                label: "My First dataset",
+                fillColor: "rgba(40, 11, 133, 0.5)",
+                strokeColor: "rgba(220, 220, 220, 0.8)",
+                highlightFill: "rgba(220, 220, 220, 0.75)",
+                highlightStroke: "rgba(50, 50, 50, 0.5)",
+                data: serie
+            }]
+        };
+        var options = {
+            scaleOverride : true,
+            scaleSteps : 10,
+            scaleStepWidth : 1000000,
+            scaleStartValue : 0,
+            responsive : true
+        };
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myNewChart = new Chart(ctx).Bar(data, options);
+    </script>
+@endsection
